91657a2f23e485c96e4168ca593b8be82c6c40e1
KAISVHORA <kaisvhora70@gmail.com>
2026-04-24 19:09:53 +0530

    react-w

--- TeamGit/src/App.jsx
+++ TeamGit/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Nav from './Nav'
 import Home from './components/Home'
+import About from './components/About'
 import Login from './Login'
 import SignUp from './SignUp'
 import Footer from './Footer'
@@ -13,6 +14,7 @@ function App() {
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
